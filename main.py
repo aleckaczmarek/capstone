01e98c90abafba5dd4cdbe197d87a6123a930000
=aleckaczmarek <=aleckaczmarek@gmail.com>
2023-01-24 23:10:34 +0000

    BASIC CRUD

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from model.Account import Account
 from repository.AccountRepository import AccountRepository
 
-account = Account(None, '045oxs', 1, 0.0)
+account = Account(21, '045oxs', 1, 5.0)
 
 repo = AccountRepository()
 
-repo.insert(account)
+repo.delete(account)
--- repository/AccountRepository.py
+++ repository/AccountRepository.py
@@ -13,18 +13,34 @@ class AccountRepository():
             cursor = dbcon.getCursor()
             cursor.execute(sql, (account.account_number, account.customer_id, account.current_balance))
             dbcon.setCommit()
+            cursor.close()
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
         finally:
             dbcon.closeConnection()
      
-    # def update(self, account: Account):
-    #     try:
-    #         sql = """INSERT INTO account(id, accountnumber, customerid, currentbalance)
-    #          VALUES(%s, %s, %s, %s) RETURNING id;"""
-    #         cursor = getCursor()
-    #         cursor.execute(sql, (account.id, account.account_number, account.customer_id, account.current_balance))
-    #     except (Exception, psycopg2.DatabaseError) as error:
-    #         print(error)
-    #     finally:
-    #         closeConnection()
+    def update(self, account: Account):
+        dbcon = DBConnect()
+        try:
+            sql = """UPDATE account SET accountnumber = %s, customerid = %s, currentbalance = %s WHERE id = %s;"""
+            cursor = dbcon.getCursor()
+            cursor.execute(sql, (account.account_number, account.customer_id, account.current_balance, account.id))
+            dbcon.setCommit()
+            cursor.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            dbcon.closeConnection()
+     
+    def delete(self, account: Account):
+        dbcon = DBConnect()
+        try:
+            sql = """DELETE FROM account WHERE id = %s;"""
+            cursor = dbcon.getCursor()
+            cursor.execute(sql, (str(account.id),))
+            dbcon.setCommit()
+            cursor.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            dbcon.closeConnection()
